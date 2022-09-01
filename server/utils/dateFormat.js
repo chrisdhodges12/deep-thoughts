21ef76ce87a7132934b453547c970cbb05d51783
chrisdhodges12 <102702357+chrisdhodges12@users.noreply.github.com>
2022-09-01 16:10:43 -0400

    setup reactions

--- server/utils/dateFormat.js
+++ server/utils/dateFormat.js
@@ -56,7 +56,7 @@ module.exports = (
     hour = 12;
   }
 
-  const minutes = dateObj.getMinutes();
+  const minutes = (dateObj.getMinutes() < 10 ? '0' : '') + dateObj.getMinutes();
 
   // set `am` or `pm`
   const periodOfDay = dateObj.getHours() >= 12 ? 'pm' : 'am';
